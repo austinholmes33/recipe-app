aa0bc0d871d919b6989ca95dbadf3e33a5a33978
Austin Holmes <austin.holmes4@gmail.com>
2023-04-11 14:33:56 -0600

    create user and login forms set up in forms.py

--- forms.py
+++ forms.py
@@ -1,1 +1,15 @@
 from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, IntegerField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, NumberRange
+
+
+class CreateUserForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email(), Length(max=100)])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=3, max=100)])
+    submit = SubmitField()
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email(), Length(max=100)])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=3, max=100)])
+    submit = SubmitField()
+
